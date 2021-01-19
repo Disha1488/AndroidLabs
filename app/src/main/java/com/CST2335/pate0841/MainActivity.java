15d0538198555920e1be1a5507b09599776419a5
pate0841 <pate0841@algonquinlive.com>
2021-01-19 13:20:09 -0500

    Initial commit

---
+++ app/src/main/java/com/CST2335/pate0841/MainActivity.java
@@ -0,0 +1,14 @@
+package com.CST2335.pate0841;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
